e8f3871e97fd0508922c4444ca16213ac1fdb826
menganha <34135296+menganha@users.noreply.github.com>
2020-05-22 06:59:43 +0200

    added bindings, fixed rules

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:pixelsize=16" };
-static const char dmenufont[]       = "JetBrains Mono:pixelsize=16";
+static const char *fonts[]          = { "JetBrains Mono:pixelsize=14" };
+static const char dmenufont[]       = "JetBrains Mono:pixelsize=14";
 static const char col_gray1[]       = "#1d2021"; /* "#222222"; */ /* black */
 static const char col_gray2[]       = "#1d2021"; /* "#444444"; */ /* slightly lighter than above black */
 static const char col_gray3[]       = "#ebdbb2"; /* "#bbbbbb"; */ /* lightGray */
@@ -19,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
 };
 
 /* layout(s) */
@@ -58,6 +58,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *shutdowncmd[]  = { "dmenu_shutdown", col_gray3, col_gray1, col_gray4, col_cyan, dmenufont, NULL };
+static const char *exitDwm[]  = {"exitDwm", col_gray3, col_gray1, col_gray4, col_cyan, dmenufont, NULL };
+static const char *lockScreen[]  = {"i3_lockScreen", "lock", NULL };
+static const char *firefox[]  = {"firefox", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
@@ -96,7 +100,10 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+    { MODKEY,                       XK_z,      spawn,          {.v = lockScreen} },
+    { MODKEY|ShiftMask,             XK_m,      spawn,          {.v = firefox} },
+    { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = exitDwm} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = shutdowncmd} },
 };
 
 /* button definitions */
